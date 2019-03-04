1847f46107d8df0a5c143e79e512c0fd2b7cb49e
thsr <437808@gmail.com>
2019-03-04 14:29:36 +0100

    clean side nav bar

--- src/options/App.vue
+++ src/options/App.vue
@@ -3,42 +3,6 @@
 <vue-progress-bar></vue-progress-bar>
 
 
-    <!--=========================
-    =            nav            =
-    ==========================-->
-<!--     <v-navigation-drawer v-model="navigation.drawer" app clipped disable-resize-watcher class="grey lighten-4">
-      <v-list dense class="grey lighten-4">
-        <template v-for="(item, i) in navigation.menu.items">
-
-          <v-layout v-if="item.heading" :key="i" row align-center>
-            <v-flex xs6>
-              <v-subheader v-if="item.heading">
-                {{ item.heading }}
-              </v-subheader>
-            </v-flex>
-            <v-flex xs6 class="text-xs-right">
-              <v-btn small flat>edit</v-btn>
-            </v-flex>
-          </v-layout>
-
-          <v-divider v-else-if="item.divider" :key="i" dark class="my-3"></v-divider>
-
-          <v-list-tile v-else :key="i" @click="">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title class="grey--text">
-                {{ item.text }}
-              </v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-
-        </template>
-      </v-list>
-    </v-navigation-drawer> -->
-
-
 
     <!--=============================
     =            toolbar            =
